8b5f7ec4d18a682fd33a798f321990cf167eb34e
YusukeNomura1024 <joblesser@gmail.com>
2021-07-12 09:06:31 +0000

    [Finish] 4-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,12 +8,12 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    
+
     redirect_to post_images_path
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_055400) do
+ActiveRecord::Schema.define(version: 2021_07_12_081825) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "user_id"
+    t.string "integer"
+    t.string "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
