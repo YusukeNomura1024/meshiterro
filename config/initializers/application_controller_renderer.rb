decf359d678ac32d227e8c55e04ef1eeb88e609a
YusukeNomura1024 <joblesser@gmail.com>
2021-07-12 05:50:23 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c63841c605b261677780f6cfdba8ce208185be0fdcc17f5e06d25fbc3f22a2239a301547e2e06060eea85f09ca618af3d2752d878f933a7175455ab86f72310f'
